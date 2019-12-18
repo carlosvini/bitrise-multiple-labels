d489504493cb9d0944cb510a59b94476cc1f1026
Carlos Rodrigues <carlos@jp7.com.br>
2019-12-18 17:05:42 -0300

    Add Appium test using wdio

---
+++ Proj1/tests/sample.test.js
@@ -0,0 +1,11 @@
+describe("Running a sample test", () => {
+    // beforeEach(() => {
+    //     $("~app-root").waitForDisplayed(11000, false)
+    // });
+
+    it("Text is correct", () => {
+        const text = $("~text1").getText();
+        console.log({ text });
+        expect(text).toEqual("This is the ONE Project");
+    });
+  });
