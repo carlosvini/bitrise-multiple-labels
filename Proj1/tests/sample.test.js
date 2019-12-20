873a0aa360fff4638e97011d6e3f642ea6a66c38
Carlos Rodrigues <carlos@jp7.com.br>
2019-12-20 10:58:40 -0300

    Add more links

--- Proj1/tests/sample.test.js
+++ Proj1/tests/sample.test.js
@@ -1,3 +1,5 @@
+// https://medium.com/swlh/automation-testing-using-react-native-and-appium-on-ubuntu-ddfddc0c29fe
+// https://chase-seibert.github.io/blog/2017/01/06/appium-react-native-quickstart.html
 // https://webdriver.io/docs/api.html
 describe("Running a sample test", () => {
     beforeEach(() => {
