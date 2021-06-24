aad8c9e31bc78f6461b776846e515ba3457c14d7
Carlos Rodrigues <carlos@jp7.com.br>
2021-06-24 19:06:52 -0300

    Add BITRISE_SIGNED_APK_PATH env variable

--- Proj1/wdio.conf.android.js
+++ Proj1/wdio.conf.android.js
@@ -61,7 +61,7 @@ exports.config = {
         platformName: 'Android',
         // platformVersion: '10',
         deviceName: 'Pixel_2_API_30', // This is name of your Simulator
-        app: `${__dirname}/android/app/build/outputs/apk/debug/app-debug.apk`,
+        app: process.env.BITRISE_SIGNED_APK_PATH || `${__dirname}/android/app/build/outputs/apk/debug/app-debug.apk`,
         // appWaitActivity: 'com.proj1.MainActivity',
         // noReset: true,
         // newCommandTimeout: 240
